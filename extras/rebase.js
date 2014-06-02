5fd6a4e42c394b747bea4b7a938a2cf9fbcdd764
Petr Shevtsov <petr.shevtsov@gmail.com>
2014-06-02 22:51:07 +0400

    Better comparison graphs positions. Rebase "percent" graphs to the middle of the area

--- extras/rebase.js
+++ extras/rebase.js
@@ -6,74 +6,42 @@ Dygraph.Plugins.Rebase = (function() {
     return "Rebase Plugin";
   };
 
-  rebase.prototype.activate = function(g) {
+  rebase.prototype.activate = function() {
     return {
       willDrawChart: this.willDrawChart
     };
   };
 
   rebase.calcRebase_ = function(axis, value, logscale) {
-    return DygraphLayout.calcYNormal_.apply(null, arguments);
-  }
+    return DygraphLayout.calcYNormal_(axis, value, logscale);
+  };
+
+  rebase.getCompareValue = function(sets) {
+    return sets[0][0].y;
+  };
 
   rebase.prototype.willDrawChart = function(e) {
     var g = e.dygraph;
 
     if (g.getOption("rebase") === null) {
-      return
-    }
-
-    function isNumber(value) {
-      var type = typeof value;
-      return type == 'number' ||
-        (value && type == 'object' && toString.call(value) == numberClass) || false;
+      return;
     }
 
     var rebaseOpt = g.getOption("rebase");
+
     var p = g.plotter_;
     var sets = p.layout.points;
+    var compareValue = (rebaseOpt === "percent") ? 0.5 : rebase.getCompareValue(sets);
 
-    for (var i = 0; i < sets.length; i++) {
-      var points = sets[i];
-      var setName = p.layout.setNames[i];
-      var axis = g.axisPropertiesForSeries(setName);
-      var logscale = g.attributes_.getForSeries("logscale", setName);
-      var starty;
+    sets.map(function(points, idx) {
+      var diff = compareValue - points[0].y;
 
-      if (isNumber(rebaseOpt)) {
-        var base = rebase.calcRebase_(axis, rebaseOpt, logscale);
-      } else if (rebaseOpt === 'percent') {
-        var yRange = g.yAxisRange(0);
-        var avg = (yRange[1] - yRange[0]) / 2;
-        var base = rebase.calcRebase_(axis, avg, logscale);
-
-        g.updateOptions({
-          axes: {
-            y: {
-              axisLabelFormatter: function(y) {
-                return y + '%';
-              },
-              ticker: function(a, b, pixels, opts, dygraph, vals) {
-                // TODO
-                return Dygraph.numericTicks(a, b, pixels, opts, dygraph, vals);
-              }
-            }
-          },
-          valueRange: [-100, 100]
-        }, true);
+      if (diff) {
+        points.map(function(point) {
+          point.y = point.y + diff;
+        });
       }
-
-      var ys = points.reduce(function(p, c) {
-        p.push(c.y);
-        return p;
-      }, []);
-      var min = Math.min.apply(Math, ys);
-      var max = Math.max.apply(Math, ys) - min;
-
-      points.map(function(c) {
-        c.y = base * (c.y - min) / max;
-      });
-    }
+    });
   };
 
   return rebase;
