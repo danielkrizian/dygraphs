161a93be4ce021d8c6e709192f80839cfd7b54f0
Petr Shevtsov <petr.shevtsov@gmail.com>
2014-06-07 13:46:57 +0400

    Improvements in RebaseHandler. Code cleanup

--- extras/rebase.js
+++ extras/rebase.js
@@ -1,72 +1,97 @@
 (function() {
   "use strict";
 
-  Dygraph.DataHandlers.RebaseHandler = function(base) {
-    this.base = base;
+  Dygraph.DataHandlers.RebaseHandler = function(baseOpt) {
+    this.baseOpt = baseOpt;
   };
 
   var RebaseHandler =  Dygraph.DataHandlers.RebaseHandler;
   RebaseHandler.prototype = new Dygraph.DataHandlers.DefaultHandler();
 
-  RebaseHandler.prototype.extractSeries = function(rawData, i, options) {
-    var series = [];
-    var baseOpt = options.get("rebase");
-    var initial = rawData[0][i];
-    var logScale = options.get("logscale");
+  RebaseHandler.rebase = function(value, initial, base) {
+    if (base === "percent") {
+      return (value / initial - 1) * 100;
+    }
+    return value * base / initial;
+  };
 
-    for (var j = 0; j < rawData.length; j++) {
-      var x = rawData[j][0];
-      var point;
-      if (baseOpt === "percent") {
-        point = j ? (rawData[j][i] / initial - 1) * 100 : 0;
+  RebaseHandler.prototype.getExtremeYValues = function(series, dateWindow, options) {
+    var minY = null, maxY = null, y;
+    var firstIdx = 0, lastIdx = series.length - 1;
+    var initial = series[firstIdx][1];
+
+    for (var j = firstIdx; j <= lastIdx; j++) {
+      if (j === firstIdx) {
+        y = (this.baseOpt === "percent") ? 0 : this.baseOpt;
       } else {
-        point = j ? rawData[j][i] * baseOpt / initial : baseOpt;
+        y = RebaseHandler.rebase(series[j][1], initial, this.baseOpt);
       }
-      if (logScale) {
-        if (point <= 0) {
-          point = null;
+      if (y === null || isNaN(y))
+        continue;
+      if (maxY === null || y > maxY) {
+        maxY = y;
+      }
+      if (minY === null || y < minY) {
+        minY = y;
+      }
+    }
+    return [ minY, maxY ];
+  };
+
+  RebaseHandler.prototype.seriesToPoints = function(series, setName, boundaryIdStart){
+    var points = [];
+    var firstIdx = 0;
+    var lastIdx = series.length - 1;
+    var initial = series[firstIdx][1]; // TODO: check for null
+    for (var i = 0; i <= lastIdx; ++i) {
+      var item = series[i];
+      var yraw = item[1];
+      var yval = yraw === null ? null : Dygraph.DataHandler.parseFloat(yraw);
+      if (yval !== null) {
+        if (i === firstIdx) {
+          yval = (this.baseOpt === "percent") ? 0 : this.baseOpt;
+        } else {
+          yval = RebaseHandler.rebase(yval, initial, this.baseOpt);
         }
       }
-      series.push([ x, point ]);
+      var point = {
+        x: NaN,
+        y: NaN,
+        xval: Dygraph.DataHandler.parseFloat(item[0]),
+        yval: yval,
+        name: setName,
+        idx: i + boundaryIdStart
+      };
+      points.push(point);
     }
-
-    return series;
+    this.onPointsCreated_(series, points);
+    return points;
   };
 
 })();
 
 Dygraph.Plugins.Rebase = (function() {
   var rebase = function() {
-    this.canonicalRawData = [];
   };
 
   rebase.prototype.toString = function() {
     return "Rebase Plugin";
   };
 
-  rebase.prototype.activate = function() {
+  rebase.prototype.activate = function(g) {
+    if (g.getOption("rebase") === null) {
+      return;
+    }
     return {
       predraw: this.predraw
     };
   };
 
-  rebase.calcRebase_ = function(axis, value, logscale) {
-    return DygraphLayout.calcYNormal_(axis, value, logscale);
-  };
-
-  rebase.getCompareValue = function(sets) {
-    return sets[0][0].y;
-  };
-
   rebase.prototype.predraw = function(e) {
     var g = e.dygraph;
-    var rebaseOpt = g.getOption("rebase");
+    var baseOpt = g.getOption("rebase");
 
-    if (rebaseOpt === null) {
-      return;
-    }
-
-    if (rebaseOpt === "percent") {
+    if (baseOpt === "percent") {
       g.updateOptions(
           {
             axes: {
@@ -82,7 +107,7 @@ Dygraph.Plugins.Rebase = (function() {
           }, true);
     }
 
-    g.dataHandler_ = new Dygraph.DataHandlers.RebaseHandler(rebaseOpt);
+    g.dataHandler_ = new Dygraph.DataHandlers.RebaseHandler(baseOpt);
   };
 
   return rebase;
